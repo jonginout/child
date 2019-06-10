b7972ed1b292269b5b9435733e14921a64fb5c7e
reddy <reddy@socar.kr>
2019-06-10 14:54:18 +0900

    init

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('child')
+module.exports = [
+    {
+        title: '제목1',
+        content: '내용1',
+    },
+    {
+        title: '제목2',
+        content: '내용2',
+    },
+    {
+        title: '제목3',
+        content: '내용3',
+    },
+]
